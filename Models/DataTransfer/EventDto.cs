971696ee7450b26d384d7370976aa81c3c9db9ec
Joshua Raneri <raneri.josh@gmail.com>
2021-02-08 11:50:59 -0500

    Calendar migration

---
+++ Models/DataTransfer/EventDto.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DataTransfer
+{
+    public class EventDto
+    {
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [DisplayName("Event Location")]
+        public string Location { get; set; }
+        [DisplayName("Event Message")]
+        public string Message { get; set; }
+        [DisplayName("Start Time")]
+        public DateTime StartTime { get; set; }
+        [DisplayName("End Time")]
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ Repo/CalendarContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public class CalendarContext : DbContext
+    {
+        public CalendarContext() { }
+
+        public CalendarContext(DbContextOptions<CalendarContext> options) : base(options) { }
+    }
+}
