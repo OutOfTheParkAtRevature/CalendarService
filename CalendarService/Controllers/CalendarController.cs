2ece5e3103b89b5bb4584abecb97d0dc56b74675
Daniel Geiser <danielgeiser87@gmail.com>
2021-02-11 11:51:13 -0800

    Added controller for calendar

---
+++ CalendarService/Controllers/CalendarController.cs
@@ -0,0 +1,61 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models.DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarService
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly ILogger<CalendarController> _logger;
+        public CalendarController(ILogger<CalendarController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Calendar>> GetCalendar()
+        {
+            return await Logic.GetCalendar();
+        }
+
+        [HttpGet("events")]
+        public async Task<IEnumerable<Event>> GetMyEvents()
+        {
+            return await Logic.GetMyEvents();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Event>> CreateEvent(EventDto eventDto)
+        {
+            var ret = await Logic.CreateEvent(eventDto);
+            if (ret == null)
+            {
+                return BadRequest("Event not created");
+            }
+            return ret;
+        }
+
+        [HttpPut("events/{id}")]
+        public async Task<ActionResult<Event>> EditEvent(EventDto eventDto, string id)
+        {
+            return await Logic.EditEvent(eventDto, id);
+        }
+
+        [HttpDelete("events/{id}")]
+        public async Task<ActionResult<string>> DeleteEvent(string id)
+        {
+            return await Logic.DeleteEvent(id);
+        }
+    }
+}
