d179e7141d4894718a0657d53a9ad98c25c78bfa
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-02-18 09:17:27 -0500

    auth2.0

--- CalendarService/Startup.cs
+++ CalendarService/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,11 +8,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CalendarService
@@ -35,6 +38,7 @@ namespace CalendarService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CalendarService", Version = "v1" });
             });
 
+            var identityUrl = Configuration.GetValue<string>("IdentityUrl");
             var jwtSettings = Configuration.GetSection("JwtSettings");
             services.AddAuthentication(opt =>
             {
